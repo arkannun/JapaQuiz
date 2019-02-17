f033ad66bc1801ef6b3cd4cb55d1cbfcf359e4e7
arkannun <arkannun@gmail.com>
2019-02-17 11:04:37 -0300

    SQL do banco de dados

---
+++ _pages/gravaDados.php
@@ -0,0 +1,30 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+
+
+    $kana = $_POST['kana'];
+    $significado = $_POST['significado'];
+    $pronuncia = $_POST['pronuncia'];
+    $numTracos = $_POST['numTracos'];
+    $grade = $_POST['grade'];
+
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "japa";
+
+    $strcon = mysqli_connect($servidor,$usuario,$senha,$banco) or die('Erro ao conectar ao banco de dados');
+
+    $sql = "INSERT INTO japa VALUES ( ";
+    $sql .= "'" .$kana .  "',";
+    $sql .= "'" . $significado .  "',";
+    $sql .= "'" . pronuncia .  "',";
+    $sql .= "'" . $numTracos .  "',";
+    $sql .= "'" . $grade .  "'";
+    $sql .= ");";
+    mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
+    mysqli_close($strcon);
+
+    header('Location: cadastro.html');
+?>
